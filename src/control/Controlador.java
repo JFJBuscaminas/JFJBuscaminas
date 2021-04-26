b3f66f3b4761ec6381092ae00041861579862008
JuanPedroB <72250676+JuanPedroB@users.noreply.github.com>
2021-04-26 20:32:06 +0200

    Actualizado 26/04

--- src/control/Controlador.java
+++ src/control/Controlador.java
@@ -2,10 +2,15 @@ package control;
 
 import modelo.Densidad;
 import modelo.Dificultad;
+import modelo.GestionDatos;
 
 public class Controlador {
 	
-	public void dameValores(Densidad densidad,Dificultad dificultad) {
+	GestionDatos gestion = new GestionDatos();
 	
+	public void crearTablero(int tamaño, int minas) {
+		gestion.crearTablero(tamaño, minas);
 	}
+	
+	
 }
